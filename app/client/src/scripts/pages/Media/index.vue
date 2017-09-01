7c37194e3e434815a47bdbc5ca9d37f7971da22d
liyi <liyi@hujiang.com>
2017-09-01 15:46:23 +0800

    增加媒体组件

--- app/client/src/scripts/pages/Media/index.vue
+++ app/client/src/scripts/pages/Media/index.vue
@@ -1,26 +1,35 @@
 <template>
-  <media :reStyles="styles"></media>
+  <div>
+    <media :reStyles="styles" ref="media"></media>
+    <div>
+      <input type="text" ref="channelInput" v-model="channel" :value="channel"/>
+      <input type="button" value="切换线路" @click="switchChannel"/>
+    </div>
+  </div>
 </template>
 
 <script>
   import { componentGetClassNames } from 'utils/dom-utils'
   import media from 'ui-base/Media'
-
   import styles from './styles'
 
   const componentClass = new componentGetClassNames(styles)
 
   export default {
-    data() {
-      return {
-        styles
-      }
-    },
+    data: () => ({
+      styles,
+      channel: '' // 推流通道
+    }),
     components: {
       media
     },
     methods: {
-      cls: componentClass.getClass
+      cls: componentClass.getClass,
+      switchChannel() {
+        const { media } = this.$refs
+
+        media && this.channel && media.switchChannel(this.channel)
+      }
     }
   }
 </script>
--- app/common/ui/src/base/Media/index.vue
+++ app/common/ui/src/base/Media/index.vue
@@ -1,26 +1,108 @@
 <template>
   <div :class="cls('media-wrap')">
     Hello mediaplayer!!!
+    <video v-if="isShowVideo" :src="videoSrc" ref="video"></video>
   </div>
 </template>
 
 <script>
+import Detector from 'utils/detector/web-detector'
 import { componentGetClassNames } from 'utils/dom-utils'
 import styles from './styles'
 
 const componentClass = new componentGetClassNames(styles)
+const isSupportHLS = /^(?:ios|android)$/i.test(Detector.os.name) || /^(?:safari|edge)$/i.test(Detector.browser.name)
+
+let HLS = null
 
 export default {
   props: [
     'reStyles',
-    'src'
+    'channel'
   ],
+  data: () => ({
+    videoSrc: '',
+
+    // 计时器
+    switchT: 0,
+
+    // 状态类
+    isShowVideo: false
+  }),
   created() {
-    // 附加重写样式
-    componentClass.addStyles(this.reStyles)
+    componentClass.addStyles(this.reStyles) // 附加重写样式
+
+    if (isSupportHLS) { // 假如支持HLS协议，则设置video.src
+      this.videoSrc = this.channel || ''
+    }
+
+    if (this.channel) { // 有推流通道才显示video
+      this.isShowVideo = true
+    }
   },
   methods: {
-    cls: componentClass.getClass
+    cls: componentClass.getClass, // 设置样式方法
+    useHLSPlugin(channel) {
+      const { video } = this.$refs
+
+      if(HLS.isSupported()) {
+        if (this.hls) { // 销毁已有的hls
+          this.hls.destroy()
+
+          if (this.hls.bufferTimer) {
+            clearInterval(this.hls.bufferTimer)
+            this.hls.bufferTimer = undefined
+          }
+
+          this.hls = null
+        }
+      }
+
+      this.hls = this.hls || new HLS()
+
+      const hls = this.hls
+
+      hls.loadSource(channel)
+      hls.attachMedia(video)
+
+      hls.on(HLS.Events.MANIFEST_PARSED, () => {
+        hls.startLoad()
+      })
+
+      hls.on(HLS.Events.ERROR, function (event, data) {
+        if (data.fatal) {
+          switch (data.type) {
+          case HLS.ErrorTypes.NETWORK_ERROR: // try to recover network error
+            hls.startLoad()
+            break;
+          case HLS.ErrorTypes.MEDIA_ERROR:
+            hls.recoverMediaError();
+            break;
+          default: // cannot recover
+            hls.destroy();
+            break;
+          }
+        }
+      })
+    },
+    switchChannel(channel) { // 切换视频地址
+      if (!channel) return
+
+      this.isShowVideo = true
+
+      if (isSupportHLS) {
+        this.videoSrc = channel
+      } else {
+        HLS
+        ? this.useHLSPlugin(channel)
+        : import('hls.js').then($hls => {
+          HLS = $hls
+          this.useHLSPlugin(channel)
+        }).catch(e => {
+          // TODO: 错误处理
+        })
+      }
+    }
   }
 }
 </script>
